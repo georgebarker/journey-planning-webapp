4e6522cc628a8d7af3cdf8d8eccaa3e3e8e0722f
georgebarker <georgebarker1@me.com>
2018-12-23 23:58:57 +0000

    User can select end location, user can specify start & end date and time.

--- app.js
+++ app.js
@@ -1,28 +1,90 @@
-angular.module('journey-planning', ['ui.select', 'ngSanitize'])
+angular.module('journey-planning', ['moment-picker', 'ui.select', 'ngSanitize'])
 .controller('journeyPlanningCtrl', function($scope, $http) {
 
+    // Define API endpoints
     const API = "http://localhost:8080/api/";
     const ENTRY_SLIP_ROAD_API = API + "entrySlipRoad/all";
-    $scope.roadNumberMap = {};
-    $scope.roadNumberArray = [];
-    $scope.junctionsForSelectedRoadNumber = [];
-    $scope.onRoadNumberSelected = onRoadNumberSelected;
+    const EXIT_SLIP_ROAD_API = API + "exitSlipRoad/all";
 
-    getRoads();
+    // Define variables
+    
+    //Slip road variables
+    $scope.entrySlipRoadMap = {};
+    $scope.entrySlipRoadArray = [];
+    $scope.junctionsForSelectedFromRoadNumber = [];
+    $scope.exitSlipRoadMap = {};
+    $scope.exitSlipRoadArray = [];
+    $scope.junctionsForSelectedToRoadNumber = [];
 
-    function onRoadNumberSelected(selectedRoadNumber) {
-        $scope.selectedJunction = null;
-        $scope.junctionsForSelectedRoadNumber = $scope.roadNumberMap[selectedRoadNumber.text];
+    //Start & end date variables
+    $scope.selectedStartDate = {};
+    $scope.selectedEndDate = {};
+    $scope.startDateHasChanged = false;
+    $scope.minStartDate = moment();
+    $scope.minEndDate = moment();
+    $scope.maxEndDate = moment();
+
+    // Define functions
+    $scope.onFromRoadNumberSelected = onFromRoadNumberSelected;
+    $scope.onToRoadNumberSelected = onToRoadNumberSelected;
+    $scope.onStartDateChanged = onStartDateChanged;
+    $scope.isPlanJourneyButtonEnabled = isPlanJourneyButtonEnabled;
+
+    // Run functions
+    getEntrySlipRoads();
+    getExitSlipRoads();
+
+    // Functions
+    function onStartDateChanged(newValue, oldValue) {
+        $scope.startDateHasChanged = true;
+        $scope.minEndDate = moment(newValue).add(15, 'minutes');
+        $scope.maxEndDate = moment($scope.selectedStartDate).add(1, 'days');
     }
 
-    function getRoads() {
+    function onFromRoadNumberSelected(selectedFromRoadNumber) {
+        $scope.selectedFromJunction = null;
+        $scope.junctionsForSelectedFromRoadNumber = $scope.entrySlipRoadMap[selectedFromRoadNumber.text];
+    }
+
+    function onToRoadNumberSelected(selectedToRoadNumber) {
+        $scope.selectedToJunction = null;
+        $scope.junctionsForSelectedToRoadNumber = $scope.exitSlipRoadMap[selectedToRoadNumber.text];
+    }
+
+    function isPlanJourneyButtonEnabled() {
+        return $scope.startDateHasChanged 
+        && $scope.selectedFromJunction != null 
+        && $scope.selectedToJunction != null;
+    }
+
+    function getEntrySlipRoads() {
         $http.get(ENTRY_SLIP_ROAD_API).
         then(function(response) {
-            $scope.roadNumberMap = response.data;
-            $scope.roadNumberArray = $.map($scope.roadNumberMap, function(value, key) {
+            $scope.entrySlipRoadMap = response.data;
+            $scope.entrySlipRoadArray = $.map($scope.entrySlipRoadMap, function(value, key) {
                 return {text: key};
             });
         });
     }
 
-});
+    function getExitSlipRoads() {
+        $http.get(EXIT_SLIP_ROAD_API).
+        then(function(response) {
+            $scope.exitSlipRoadMap = response.data;
+            $scope.exitSlipRoadArray = $.map($scope.exitSlipRoadMap, function(value, key) {
+                return {text: key};
+            });
+        });
+    }
+
+})
+.config(['momentPickerProvider', function (momentPickerProvider) {
+    momentPickerProvider.options({
+        minutesStep: 15,
+        format: "lll",
+        locale: "en-gb",
+        minView: "month",
+        maxView: "hour"
+    });
+}])
+;
