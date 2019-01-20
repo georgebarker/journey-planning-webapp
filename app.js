42467ec0d7bde1304578fbe3bc8ea435ae84e543
georgebarker <georgebarker1@me.com>
2019-01-20 17:21:51 +0000

    Tells user what their optimal journey time is along with departure and arrival.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ angular.module('journey-planning', ['moment-picker', 'ui.select', 'ngSanitize'])
 .controller('journeyPlanningCtrl', function($scope, $http) {
 
     // Define API endpoints
-    const API = "http://localhost:8080/api/";
-    const ENTRY_SLIP_ROAD_API = API + "entrySlipRoad/all";
-    const EXIT_SLIP_ROAD_API = API + "exitSlipRoad/all";
+    const STATIC_DATA_API = "http://localhost:8080/api/";
+    const JOURNEY_PLANNING_API = "http://localhost:8081/route/"
+    const ENTRY_SLIP_ROAD_API = STATIC_DATA_API + "entrySlipRoad/all";
+    const EXIT_SLIP_ROAD_API = STATIC_DATA_API + "exitSlipRoad/all";
+    const STRING_DATE_TIME_FORMAT = "hh:mm a";
 
     // Define variables
     
@@ -24,11 +26,20 @@ angular.module('journey-planning', ['moment-picker', 'ui.select', 'ngSanitize'])
     $scope.minEndDate = moment();
     $scope.maxEndDate = moment();
 
+    //Journey data variables
+    $scope.returnedRoutes = null;
+    $scope.optimalRoute = null;
+
     // Define functions
     $scope.onFromRoadNumberSelected = onFromRoadNumberSelected;
     $scope.onToRoadNumberSelected = onToRoadNumberSelected;
     $scope.onStartDateChanged = onStartDateChanged;
-    $scope.isPlanJourneyButtonEnabled = isPlanJourneyButtonEnabled;
+    // $scope.isPlanJourneyButtonDisabled = isPlanJourneyButtonDisabled;
+    $scope.getRoutes = getRoutes;
+
+    //UI Variables
+    var loadingSpinner = $('#loading-spinner');
+    var planJourneyButton = $('#plan-journey-button');
 
     // Run functions
     getEntrySlipRoads();
@@ -51,11 +62,23 @@ angular.module('journey-planning', ['moment-picker', 'ui.select', 'ngSanitize'])
         $scope.junctionsForSelectedToRoadNumber = $scope.exitSlipRoadMap[selectedToRoadNumber.text];
     }
 
-    function isPlanJourneyButtonEnabled() {
-        return $scope.startDateHasChanged 
-        && $scope.selectedFromJunction != null 
-        && $scope.selectedToJunction != null;
+    function setLoadingUI() {
+        loadingSpinner.show();
+        planJourneyButton.text('Loading...');
+        planJourneyButton.prop('disabled', true);
     }
+
+    function removeLoadingUI() {
+        loadingSpinner.hide();
+        planJourneyButton.text('Plan my journey');
+        planJourneyButton.prop('disabled', false);
+    }
+
+    /* function isPlanJourneyButtonDisabled() {
+        return !$scope.startDateHasChanged 
+        || $scope.selectedFromJunction == null 
+        || $scope.selectedToJunction == null;
+    } */
 
     function getEntrySlipRoads() {
         $http.get(ENTRY_SLIP_ROAD_API).
@@ -75,6 +98,61 @@ angular.module('journey-planning', ['moment-picker', 'ui.select', 'ngSanitize'])
                 return {text: key};
             });
         });
+    }
+
+    function getRoutes() {
+        setLoadingUI();
+        var URL = JOURNEY_PLANNING_API 
+        + $scope.selectedFromJunction.linkId 
+        + "/" 
+        + $scope.selectedToJunction.linkId
+        + "/"
+        + $scope.selectedStartDate
+        + "/"
+        + $scope.selectedEndDate;
+
+        $http.get(URL).
+        then(function(response) {
+            removeLoadingUI();
+            $scope.returnedRoutes = response.data;
+            setOptimalRoute();
+        });
+    }
+
+    function setOptimalRoute() {
+        $.each($scope.returnedRoutes, function(index, route) {
+            if (route.optimalRoute) {
+                $scope.optimalRoute = route;
+                return false;
+            }
+        });
+        setTravelTimeText();
+        setDepartureArrivalTimeText();
+    }
+
+    function setDepartureArrivalTimeText() {
+        var departureTime = moment($scope.optimalRoute.departureTimeMillis);
+        var arrivalTime = moment($scope.optimalRoute.arrivalTimeMillis);
+        $scope.optimalRoute.departureTimeText = departureTime.format(STRING_DATE_TIME_FORMAT);
+        $scope.optimalRoute.arrivalTimeText = arrivalTime.format(STRING_DATE_TIME_FORMAT);
+    }
+
+    function setTravelTimeText() {
+        var travelTimeText = null;
+        var minutes = Math.round($scope.optimalRoute.minutesToTravel);
+        var hours = Math.floor(minutes / 60);
+
+        if (hours > 0) {
+            var minutesOfTheHour = minutes - (hours * 60);
+            if (minutesOfTheHour == 0) {
+                travelTimeText = hours + " hours";
+            } else {
+                travelTimeText = hours + " hours, " + minutesOfTheHour + " minutes";
+            }
+        } else {
+            travelTimeText = minutes + " minutes";
+        }
+        $scope.optimalRoute.travelTimeText = travelTimeText;
     }
 
 })
